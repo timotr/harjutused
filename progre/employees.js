20c91d8ecb217ba4dfcabfad6945801bf99c855f
Timo Triisa <timotriisa@gmail.com>
2022-10-08 11:39:10 +0300

    Create employees.js

---
+++ progre/employees.js
@@ -0,0 +1,13 @@
+const employees = [
+    { name: 'John', age: 30, salary: 30000 },
+    { name: 'Mary', age: 28, salary: 15000 },
+    { name: 'Steve', age: 50, salary: 50000 },
+    { name: 'Bill', age: 35, salary: 20000 },
+    { name: 'Mike', age: 40, salary: 45000 },
+    { name: 'Susan', age: 35, salary: 20000 },
+    { name: 'Marry', age: 40, salary: 45000 },
+    { name: 'Tom', age: 50, salary: 50000 },
+    { name: 'Jerry', age: 28, salary: 15000 },
+    { name: 'Tim', age: 30, salary: 30000 },
+    { name: 'John', age: 30, salary: 30000 }
+];
