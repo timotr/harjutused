996e4fab9e24b1eb402e986bb2ca1345cd1b65bc
Timo Triisa <timotriisa@gmail.com>
2023-11-13 16:22:56 +0200

    Delete checkTemp.js

--- checkTemp.js
+++
@@ -1,9 +0,0 @@
-for(let i = 4**26; i < 5**26; i++) {
-    let result = "";
-    let j = i;
-    while(j > 0) {
-        result += String.fromCharCode(97 + j % 6);
-        j = Math.floor(j / 6);
-    }
-    this[result] = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]
-}
